1c7546833ad8427e3591258c64eb77a5f5799f60
Kreato <hi@krea.to>
2026-07-16 13:04:56 +0300

    test: verify Loki hint action order

--- .github/alloy/test-kpkg-telemetry.sh
+++ .github/alloy/test-kpkg-telemetry.sh
@@ -3,10 +3,44 @@ set -eu
 
 config=${1:-.github/alloy/kpkg-telemetry.alloy}
 
-test "$(grep -c 'key    = "loki.resource.labels"' "$config")" -eq 2
-test "$(grep -c 'key    = "loki.attribute.labels"' "$config")" -eq 2
-test "$(grep -c 'action = "delete"' "$config")" -eq 2
-grep -F 'value  = "service.name"' "$config"
-grep -F 'value  = "span_name,status,package_name,run3_stage"' "$config"
+assert_hint_actions() {
+  hint=$1
+  value=$2
+
+  actions=$(awk -v hint="$hint" '
+    /^[[:space:]]*action[[:space:]]*\{/ {
+      in_action = 1
+      key = op = val = ""
+      next
+    }
+    in_action && /^[[:space:]]*key[[:space:]]*=/ {
+      key = $3
+      gsub(/"/, "", key)
+      next
+    }
+    in_action && /^[[:space:]]*action[[:space:]]*=/ {
+      op = $3
+      gsub(/"/, "", op)
+      next
+    }
+    in_action && /^[[:space:]]*value[[:space:]]*=/ {
+      val = $3
+      gsub(/"/, "", val)
+      next
+    }
+    in_action && /^[[:space:]]*\}/ {
+      if (key == hint) {
+        print op "=" val
+      }
+      in_action = 0
+    }
+  ' "$config")
+
+  test "$actions" = "delete=
+insert=$value"
+}
+
+assert_hint_actions "loki.resource.labels" "service.name"
+assert_hint_actions "loki.attribute.labels" "span_name,status,package_name,run3_stage"
 ! grep -Eq 'trace(_id|\.id)|span(_id|\.id)' "$config"
 grep -F 'otelcol.exporter.loki "grafana_cloud" {' "$config"
